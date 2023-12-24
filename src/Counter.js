afebd5b436b2df6c6ba2ca06298a3d7d1fb26941
osman <osmnlhn@gmail.com>
2023-12-24 03:08:46 +0000

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,34 @@
+import react, { useReducer } from "react";
+
+const initialState = {
+  count: 0,
+};
+
+const reducer = (state, action) => {
+  if (!action) return state;
+
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+
+    case "decrement":
+      return { count: state.count - 1 };
+
+    case "increment_by":
+      return { count: state.count + action.payload };
+
+    default:
+      return state;
+  }
+};
+
+export default function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <>
+      <div className="count">Count:{state.count}</div>
+      <button onClick={() => dispatch({ type: "increment" })}>+1</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-1</button>
+    </>
+  );
+}
